2914c6e11e56609b2ad725d7a60bc32922acf807
Hayolox <akiklii72@gmail.com>
2022-04-27 03:01:46 +0700

    update sign

--- lib/screen/sign/sign_screen.dart
+++ lib/screen/sign/sign_screen.dart
@@ -1,16 +1,15 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:delayed_display/delayed_display.dart';
 import 'package:lottie/lottie.dart';
 import 'package:moneyku/screen/sign/sign_view_model.dart';
 import 'package:provider/provider.dart';
-
 import '../../theme.dart';
 
 // ignore: must_be_immutable
 class SignScreen extends StatelessWidget {
   SignScreen({Key? key}) : super(key: key);
-  TextEditingController emailC = TextEditingController();
-  TextEditingController passwordC = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final isKeyboard = MediaQuery.of(context).viewInsets.bottom != 0;
@@ -18,165 +17,153 @@ class SignScreen extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: <Widget>[
-              if (!isKeyboard)
+          child: Form(
+              key: _formKey,
+              child: Consumer<SignViewModel>(
+                builder: (context, value, child) {
+                  return Column(
+                    children: <Widget>[
+                      if (!isKeyboard)
 
-                /// Logo
-                DelayedDisplay(
-                  delay: const Duration(seconds: 1),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Lottie.network(
-                      'https://assets10.lottiefiles.com/packages/lf20_gjmecwii.json',
-                      height: 300,
-                    ),
-                  ),
-                ),
-              if (isKeyboard)
-                const SizedBox(
-                  height: 100,
-                ),
-
-              /// Form NIM
-              DelayedDisplay(
-                  delay: const Duration(seconds: 2),
-                  child: Container(
-                    margin: const EdgeInsets.only(
-                      left: 30,
-                      right: 30,
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 15,
-                    ),
-                    height: 60,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Row(
-                      children: [
-                        const Icon(
-                          Icons.mail_outline,
-                          color: Color.fromARGB(255, 248, 76, 76),
+                        /// Logo
+                        DelayedDisplay(
+                          delay: const Duration(seconds: 1),
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 30),
+                            child: Lottie.network(
+                              'https://assets10.lottiefiles.com/packages/lf20_gjmecwii.json',
+                              height: 300,
+                            ),
+                          ),
                         ),
+                      if (isKeyboard)
                         const SizedBox(
-                          width: 4,
+                          height: 100,
                         ),
-                        Expanded(
-                            child: TextFormField(
-                          autocorrect: false,
-                          controller: emailC,
-                          textInputAction: TextInputAction.next,
-                          decoration: const InputDecoration.collapsed(
-                            hintText: 'contoh@gmail.com',
-                          ),
-                        ))
-                      ],
-                    ),
-                  )),
 
-              ///Form password
-              DelayedDisplay(
-                  delay: const Duration(seconds: 3),
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 30, right: 30, top: 15),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 15,
-                    ),
-                    height: 60,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Center(child: Consumer<SignViewModel>(
-                      builder: (context, value, child) {
-                        return Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(
-                              Icons.key,
-                              color: Color.fromARGB(255, 248, 76, 76),
-                            ),
-                            const SizedBox(
-                              width: 4,
-                            ),
-                            Expanded(
-                                child: TextFormField(
-                              autocorrect: false,
-                              controller: passwordC,
-                              textInputAction: TextInputAction.done,
-                              obscureText: value.isHideen,
-                              decoration: const InputDecoration.collapsed(
-                                hintText: '',
+                      /// Form Email
+                      DelayedDisplay(
+                        delay: const Duration(seconds: 2),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 30, right: 30),
+                          child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              textInputAction: TextInputAction.next,
+                              controller: value.emailC,
+                              decoration: const InputDecoration(
+                                floatingLabelStyle:
+                                    TextStyle(color: Colors.red),
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                ),
+                                contentPadding: EdgeInsets.only(left: 40),
+                                prefixIcon: Icon(
+                                  Icons.mail_outline,
+                                  color: Color.fromARGB(255, 248, 76, 76),
+                                ),
+                                labelText: 'Email',
+                                hintText: 'Masukkan Email',
                               ),
-                            )),
-                            IconButton(
-                                padding: const EdgeInsets.all(0),
-                                onPressed: () {
-                                  value.changeIsHidden(value.isHideen);
-                                },
-                                icon:
-                                    const Icon(Icons.remove_red_eye_outlined)),
-                          ],
-                        );
-                      },
-                    )),
-                  )),
-              const SizedBox(
-                height: 10,
-              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Email Tidak Boleh Kosong';
+                                }
+                                final bool _isValid = EmailValidator.validate(
+                                  value.toString(),
+                                );
+                                return _isValid == false
+                                    ? 'Format harus berupa email'
+                                    : null;
+                              }),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
 
-              DelayedDisplay(
-                  delay: const Duration(seconds: 4),
-                  child: Consumer<SignViewModel>(
-                    builder: (context, value, child) {
-                      return GestureDetector(
-                        onTap: () {
-                          Object condition = value.validation(
-                              emailC.text, passwordC.text, context);
-                          if (condition == 'success') {
-                            print('login berhasil');
-                            Navigator.pushReplacementNamed(context, '/home');
-                          }
-                        },
-
-                        ///button login
-                        child: Container(
-                          height: 50,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.topRight,
-                                end: Alignment.bottomLeft,
-                                colors: [
-                                  backroundColor1,
-                                  backroundColor2,
-                                ]),
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          child: Center(
-                            child: Text('Login',
-                                style: primaryTextStyle.copyWith(
-                                  fontSize: 20,
-                                )),
+                      ///Form password
+                      DelayedDisplay(
+                        delay: const Duration(seconds: 3),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 30, right: 30),
+                          child: TextFormField(
+                            controller: value.passwordC,
+                            obscureText: value.isHideen,
+                            decoration: InputDecoration(
+                              floatingLabelStyle:
+                                  const TextStyle(color: Colors.red),
+                              border: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                              ),
+                              prefixIcon: const Icon(
+                                Icons.key,
+                                color: Color.fromARGB(255, 248, 76, 76),
+                              ),
+                              suffixIcon: GestureDetector(
+                                  onTap: () {
+                                    value.changeIsHidden(value.isHideen);
+                                  },
+                                  child: const Icon(Icons.remove_red_eye)),
+                              contentPadding: const EdgeInsets.only(left: 40),
+                              labelText: 'Password',
+                              hintText: 'Masukkan Password',
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Password Tidak Boleh Kosong';
+                              }
+                              return null;
+                            },
                           ),
                         ),
-                      );
-                    },
-                  ))
-            ],
-          ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+
+                      DelayedDisplay(
+                          delay: const Duration(seconds: 4),
+                          child: Consumer<SignViewModel>(
+                            builder: (context, value, child) {
+                              return GestureDetector(
+                                onTap: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    _formKey.currentState!.save();
+                                    Navigator.of(context)
+                                        .pushReplacementNamed('/home');
+                                  }
+                                },
+
+                                ///button login
+                                child: Container(
+                                  height: 50,
+                                  width: 150,
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        begin: Alignment.topRight,
+                                        end: Alignment.bottomLeft,
+                                        colors: [
+                                          backroundColor1,
+                                          backroundColor2,
+                                        ]),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: Center(
+                                    child: Text('Login',
+                                        style: primaryTextStyle.copyWith(
+                                          fontSize: 20,
+                                        )),
+                                  ),
+                                ),
+                              );
+                            },
+                          ))
+                    ],
+                  );
+                },
+              )),
         ),
       ),
     );
--- lib/screen/sign/sign_view_model.dart
+++ lib/screen/sign/sign_view_model.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
-import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:email_validator/email_validator.dart';
 
 class SignViewModel extends ChangeNotifier {
   bool isHideen = true;
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passwordC = TextEditingController();
 
   void changeIsHidden(bool paramIsHidden) {
     if (paramIsHidden == true) {
@@ -12,38 +12,5 @@ class SignViewModel extends ChangeNotifier {
       isHideen = true;
     }
     notifyListeners();
-  }
-
-  Object validation(
-    String paramEmail,
-    String paramPassword,
-    BuildContext context,
-  ) {
-    if (paramEmail.isEmpty && paramPassword.isEmpty) {
-      return seeDialog(
-          'Email & Password  tidak boleh kosong', DialogType.ERROR, context);
-    } else if (paramEmail.isEmpty) {
-      return seeDialog('Email  tidak boleh kosong', DialogType.ERROR, context);
-    } else if (paramPassword.isEmpty) {
-      return seeDialog(
-          'Password  tidak boleh kosong', DialogType.ERROR, context);
-    } else {
-      final bool isValid = EmailValidator.validate(paramEmail);
-
-      return isValid == false
-          ? seeDialog('Format email salah', DialogType.ERROR, context)
-          : 'success';
-    }
-  }
-
-  AwesomeDialog seeDialog(
-      String desc, DialogType dialogType, BuildContext context) {
-    return AwesomeDialog(
-        context: context,
-        animType: AnimType.SCALE,
-        dialogType: dialogType,
-        desc: desc,
-        btnOkOnPress: () {})
-      ..show();
   }
 }
