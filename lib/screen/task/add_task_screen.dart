ea573dcc4d69932a54772212aec9a73e7dc18402
Hayolox <akiklii72@gmail.com>
2022-04-27 16:10:10 +0700

    add task

---
+++ lib/screen/task/add_task_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:moneyku/screen/notes/formater_screen.dart';
+import 'package:moneyku/screen/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  AddTaskScreen({Key? key}) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tambah Task'),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: const EdgeInsets.only(right: 15, left: 15, top: 30),
+          child: Form(
+              key: _formKey,
+              child: Consumer<NotesViewModel>(
+                builder: (context, value, child) {
+                  return Column(
+                    children: [
+                      TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          textInputAction: TextInputAction.next,
+                          decoration: const InputDecoration(
+                            floatingLabelStyle: TextStyle(color: Colors.green),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                            ),
+                            contentPadding: EdgeInsets.only(left: 40),
+                            prefixIcon: Icon(
+                              Icons.title,
+                            ),
+                            labelText: 'Judul Task',
+                            hintText: 'Isi Judul Task',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Task Tidak Boleh Kosong';
+                            }
+                            return null;
+                          }),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                          keyboardType: TextInputType.number,
+                          textInputAction: TextInputAction.next,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                            CurrencyPtBrInputFormatter()
+                          ],
+                          decoration: const InputDecoration(
+                            floatingLabelStyle: TextStyle(color: Colors.green),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                            ),
+                            contentPadding: EdgeInsets.only(left: 40),
+                            prefixIcon: Icon(
+                              Icons.arrow_upward,
+                            ),
+                            labelText: 'Harga Task',
+                            hintText: 'Isi harga task',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Harga Task Tidak Boleh Kosong';
+                            }
+                            return null;
+                          }),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          value.showDate(context);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 10,
+                          ),
+                          height: 40,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              const SizedBox(
+                                width: 4,
+                              ),
+                              const Icon(
+                                Icons.date_range,
+                                size: 20,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                DateFormat('dd-MM-yyy').format(value.dueDate),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Center(
+                          child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                            print(value.currenDate);
+                          }
+                        },
+                        child: const Text('Submit'),
+                      ))
+                    ],
+                  );
+                },
+              )),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/task/task_screen.dart
+++ lib/screen/task/task_screen.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:moneyku/screen/task/add_task_screen.dart';
+import 'package:transition/transition.dart';
 
 class Taskcreen extends StatelessWidget {
   const Taskcreen({Key? key}) : super(key: key);
 
+  Widget appBar() {
+    return AppBar(
+      title: const Text('Todo List'),
+      centerTitle: true,
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 15,
+          left: 20,
+          right: 20,
+        ),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ListTile(
+              onTap: () {
+                print('tes');
+              },
+              title: const Text('Diesnatalis Universita Palangkaraya 3x5'),
+              subtitle: const Text('12 april 2021'),
+              trailing: const Text('Rp 40.000'),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Taskcreen'),
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(bottom: 10),
+        child: Column(
+          children: [
+            appBar(),
+            content(),
+            Center(
+                child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  Transition(
+                      child: AddTaskScreen(),
+                      transitionEffect: TransitionEffect.RIGHT_TO_LEFT),
+                );
+              },
+              child: const Text('Tambah Taks'),
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
